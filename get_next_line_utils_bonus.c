867f16c7c37d2b1453084567eb0f22a57f51d634
Jacquet BENJAMIN <jabenjam@e2r9p16.42.fr>
2020-02-24 17:34:50 +0100

    imported from drive

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,92 @@
+
+#include "get_next_line_bonus.h"
+#include <unistd.h>
+#include <stdio.h>
+
+size_t	ft_strlenc(char *str, char c)
+{
+	unsigned int		i;
+
+	i = 0;
+	while (str[i] != c && str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+	char			*new;
+
+	i = ft_strlenc(s1, '\0') + ft_strlenc(s2, '\0');
+	j = 0;
+	if (!(new = malloc(sizeof(char) * i + 1)))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		new[i + j] = s2[j];
+		j++;
+	}
+	new[i + j] = '\0';
+	return (new);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*new;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (!(new = malloc(sizeof(char) * len + 1)))
+		return (0);
+	while (s[i + start] && i < len && start < len)
+	{
+		new[i] = s[i + start];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int		i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int		ft_findc(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (c == '\0')
+	{
+		while (s[i] != '\0')
+			i++;
+		return (1);
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
